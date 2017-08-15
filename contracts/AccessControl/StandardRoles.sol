7a7185a6647b6db0ee76232089e47c079fb6ef3e
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-15 15:19:44 +0200

    Add AccessControl to Curve

--- contracts/AccessControl/StandardRoles.sol
+++ contracts/AccessControl/StandardRoles.sol
@@ -4,5 +4,5 @@ contract StandardRoles {
 
     // NOTE: Soldity somehow doesn't evaluate this compile time
     bytes32 public constant ROLE_ACCESS_CONTROLER = keccak256("AccessControler");
-    bytes32 public constant ROLE_OWNER = keccak256("Owner");
+    bytes32 public constant ROLE_ADMINISTRATOR = keccak256("Administrator");
 }
