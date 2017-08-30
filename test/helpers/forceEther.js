911a9dc8b32849e61d638d2b1725be569473c5c3
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-30 22:05:52 +0200

    can forceEther from any account

--- test/helpers/forceEther.js
+++ test/helpers/forceEther.js
@@ -1,6 +1,6 @@
 const ForceEther = artifacts.require("ForceEther");
 
-export default async (address, amountWei) => {
-  const forceEther = await ForceEther.new({ value: amountWei });
+export default async (address, amountWei, fromAddress) => {
+  const forceEther = await ForceEther.new({ value: amountWei, from: fromAddress });
   await forceEther.pay(address);
 };
