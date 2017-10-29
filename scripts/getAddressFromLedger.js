92882e252b1c463136781a4de84d57f9a148f4ce
Tomek Banach <tomek@banciur.org>
2017-10-29 10:30:32 +0100

    getAddressFromLedger.js lint fixes

--- scripts/getAddressFromLedger.js
+++ scripts/getAddressFromLedger.js
@@ -12,11 +12,17 @@ const DEFAULT_PATH = "44'/60'/0'/0";
 const path = process.argv[2] || DEFAULT_PATH;
 console.log("checking path:", path);
 
-ledger.comm_node.create_async(TIMEOUT).then(comm => {
-  // eslint-disable-next-line new-cap
-  const eth = new ledger.eth(comm);
-  return eth.getAddress_async(path).then(result => {
-    console.log(result);
-    comm.close_async();
-  }).catch(err => console.log(err));
-}).catch(err => console.log(err));
+ledger.comm_node
+  .create_async(TIMEOUT)
+  .then(comm => {
+    // eslint-disable-next-line new-cap
+    const eth = new ledger.eth(comm);
+    return eth
+      .getAddress_async(path)
+      .then(result => {
+        console.log(result);
+        comm.close_async();
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
